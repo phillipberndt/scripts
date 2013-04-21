af2ca1732565ac52c0d7bb8bf5a6841f20e93ab0
Phillip Berndt <phillip.berndt@gmail.com>
2013-04-21 11:52:45 +0200

    add_package: Support for non-/ root

--- nonroot_apt/add_package.py
+++ nonroot_apt/add_package.py
@@ -9,11 +9,13 @@ import os
 import shutil
 import sys
 
-def files(package):
-	return ( x.strip() for x in os.popen("dpkg -L " + package).readlines() )
+def files(package, source):
+	return ( x.strip() for x in os.popen("dpkg --admindir=" + source + "/var/lib/dpkg -L " + package).readlines() )
 
-def install(file, target_base):
-	if not os.path.isfile(file):
+def install(file, source, target_base):
+	real_file = os.path.join(source, "." + file)
+
+	if not os.path.isfile(real_file):
 		return
 	target = os.path.join(target_base, "./" + file)
 	try:
@@ -21,42 +23,47 @@ def install(file, target_base):
 	except:
 		pass
 	if not os.access(target, os.F_OK):
-		shutil.copy2(file, target)
+		shutil.copy2(real_file, target)
 
-def dependencies(package):
-	return ( x[x.find(": ") + 2:].strip().replace("<", "").replace(">", "") for x in os.popen("apt-cache depends " + package).readlines() if "Depends" in x )
+def dependencies(package, source):
+	return ( x[x.find(": ") + 2:].strip().replace("<", "").replace(">", "") for x in os.popen("apt-cache -o RootDir=" + source + " depends " + package).readlines() if "Depends" in x )
 
 _deps = []
 
-def _install_single_package(package, target_base):
+def _install_single_package(package, source, target_base):
 	global _deps
-	for dep in dependencies(package):
+	for dep in dependencies(package, source):
 		if dep in _deps:
 			continue
 		_deps += [ dep ]
-		_install_single_package(dep, target_base)
+		_install_single_package(dep, source, target_base)
 	print "Installing", package, "..."
-	for file in files(package):
-		install(file, target_base)
+	for file in files(package, source):
+		install(file, source, target_base)
 
-def install_packages(packages, target_base):
+def install_packages(packages, source, target_base):
 	global _deps
 	_deps = []
 	if type(packages) is str:
 		packages = [ packages ]
 	for package in packages:
-		_install_single_package(package, target_base)
+		_install_single_package(package, source, target_base)
 
 if __name__ == '__main__':
 	try:
-		args, params = getopt.getopt(sys.argv[1:], "d:")
+		args, params = getopt.getopt(sys.argv[1:], "s:d:")
 	except:
-		print "Syntax: add_package [-d root] package(s)..."
+		print "Syntax: add_package [-s source] [-d root] package(s)..."
 		sys.exit(0)
 
-	if len(args) > 0:
-		target_base = args[0][1]
+	args = dict(args)
+	if "-d" in args:
+		target_base = args["-d"]
 	else:
 		target_base = "./"
+	if "-s" in args:
+		source = args["-s"]
+	else:
+		source = "/"
 
-	install_packages(params, target_base)
+	install_packages(params, source, target_base)
