6f3839065564b33d6ac83013c7bd3a35ab3a76dc
Phillip Berndt <phillip.berndt@googlemail.com>
2015-04-15 11:37:30 +0200

    on: CPU usage, nicer output

--- on/on.py
+++ on/on.py
@@ -32,20 +32,24 @@ class OnPIDExit(OnEvent):
             if not OnPIDExit.is_alive(pid_list[0]):
                 raise ValueError("PID %d does not belong to an existing process" % pid_list[0])
         except ValueError:
-            processes = map(int, subprocess.check_output(["pgrep", "-f", parameter]).split())
-            if os.getpid() in processes:
-                processes.remove(os.getpid())
+            processes = dict((x.strip().split(None, 1) for x in subprocess.check_output(["pgrep", "-lf", parameter]).split("\n") if x.strip()))
+            if str(os.getpid()) in processes:
+                del processes[str(os.getpid())]
             if not processes:
                 raise ValueError("No process matches `%s'" % parameter)
             if len(processes) > 1:
-                print "Warning: `%s' matches %d processes." % (parameter, len(processes))
-            pid_list = processes
+               status(1, "PID", "Multiple choices: ")
+               for pid, proc in processes.items():
+                   print "      %04s %s" % (pid, proc)
+            pid_list = map(int, processes.keys())
+            status(0, "PID", "Waiting for any of %d processes to exit" % len(pid_list))
         self.pid_list = pid_list
 
     def wait_for_event(self):
         while True:
             for pid in self.pid_list:
                 if not OnPIDExit.is_alive(pid):
+                    status(0, "PID", "PID %d exited" % pid, True)
                     return
             time.sleep(1)
 # }}}
@@ -64,15 +68,19 @@ if has_pyinotify:
 
         def __init__(self, parameter):
             self.wm = pyinotify.WatchManager()
-            for file_name in glob.glob(parameter):
+            files = glob.glob(parameter)
+            for file_name in files:
                 self.wm.add_watch(file_name, pyinotify.IN_CLOSE_WRITE, rec=True)
+            status(0, "Inotify", "Watching %d files" % len(files))
 
         def wait_for_event(self):
             notifier = pyinotify.Notifier(self.wm)
             notifier.process_events()
             notifier.check_events()
             notifier.read_events()
-            return notifier._sys_proc_fun(notifier._eventq[0]).pathname
+            path = notifier._sys_proc_fun(notifier._eventq[0]).pathname
+            status(0, "Inotify", "%s updated" % path, True)
+            return path
 # }}}
 # Network throughput {{{
 class NetworkThroughput(OnEvent):
@@ -97,13 +105,51 @@ class NetworkThroughput(OnEvent):
         self.nbytes = NetworkThroughput.get_bytes()
         self.time = time.time()
         time.sleep(1)
-        print "Info: Startup throughput is %2.2f kiB/s" % (self.get_throughput() / 1024)
+        status(0, "Network Throughput", "Startup throughput is %2.2f kiB/s" % (self.get_throughput() / 1024, ))
 
     def wait_for_event(self):
         while True:
             time.sleep(10)
             throughput = self.get_throughput()
+            status(0, "Network Throughput", "Throughput is %2.2f kiB/s" % (throughput, ), True)
             if throughput < self.threshold:
+                break
+# }}}
+# CPU usage {{{
+class CPUUsage(OnEvent):
+    DESCRIPTION = "CPU usage drops below a threshold"
+    PREFIX = "cpu"
+    PARAMETER_DESCRIPTION = "percent"
+
+    @staticmethod
+    def get_stat():
+        stat  = open("/proc/stat").readline().split()
+        assert stat[0] == "cpu"
+        user, nice, system, idle = stat[1:5]
+        work_time = int(user) + int(nice) + int(system)
+        return work_time, work_time + int(idle)
+
+    def get_usage(self):
+        stat = CPUUsage.get_stat()
+        usage = (stat[0] - self.stat[0]) * 100. / (stat[1] - self.stat[1])
+        self.stat = stat
+        return usage
+
+    def __init__(self, parameter):
+        self.threshold = float(parameter)
+        self.stat = CPUUsage.get_stat()
+        time.sleep(1)
+        status(0, "CPU usage", "Startup CPU usage is %2.2f%%" % (self.get_usage(),))
+
+    def wait_for_event(self):
+        while True:
+            time.sleep(1)
+            try:
+                usage = self.get_usage()
+            except ZeroDivisionError:
+                continue
+            status(0, "CPU usage", "CPU usage is %2.2f%%" % (usage,), True)
+            if usage < self.threshold:
                 break
 # }}}
 
@@ -120,13 +166,23 @@ def print_help():
     for cls in OnEvent.__subclasses__():
         print "  %-20s %s" % ("%s:<%s>" % (cls.PREFIX, cls.PARAMETER_DESCRIPTION) if cls.PARAMETER_DESCRIPTION else cls.PREFIX, cls.DESCRIPTION)
 
+def status(level, component, line, is_update=False):
+    if level == 0:
+        col = "32"
+    elif level == 1:
+        col = "33"
+    else:
+        col = "31"
+    up = "" if not is_update else "\033[1F\033[K"
+    print "%s[\033[%sm%s\033[0m] %s" % (up, col, component, line)
+
 def is_executable(file_name):
     for path in os.environ["PATH"].split(":"):
         if os.access(os.path.join(path, file_name), os.X_OK | os.F_OK):
             return True
     return False
 
-if __name__ == '__main__':
+def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "krw")
         opts = dict(opts)
@@ -155,13 +211,20 @@ if __name__ == '__main__':
         instance.wait_for_event()
         if "-k" in opts and proc:
             if proc.poll() is None:
-                print "Info: Killing old action instance %d" % proc.pid
+                status(1, "on", "Killing old action instance %d" % proc.pid)
                 proc.kill()
-        if is_executable(action[0]):
-            proc = subprocess.Popen(action)
-        else:
-            proc = subprocess.Popen(action, shell=True)
-        if "-w" in opts:
-            proc.wait()
+        if action:
+            if is_executable(action[0]):
+                proc = subprocess.Popen(action)
+            else:
+                proc = subprocess.Popen(action, shell=True)
+            if "-w" in opts:
+                proc.wait()
         if "-r" not in opts:
             break
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
