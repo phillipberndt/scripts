33f7021cf1de681df1b37f2911766a56b86070c5
Phillip Berndt <phillip.berndt@googlemail.com>
2015-05-27 19:47:59 +0200

    pskill: Non-interactive alternative mode and neater output

--- pskill/pskill.py
+++ pskill/pskill.py
@@ -10,16 +10,19 @@ import termios
 import time
 
 TEXT_RED = "\033[31m"
+TEXT_GREEN = "\033[32m"
+TEXT_YELLOW = "\033[33m"
 TEXT_BOLD = "\033[1m"
 TEXT_HALF_BRIGHT = "\033[2m"
 TEXT_DEFAULT = "\033[0m"
 TEXT_ERASE = "\033[H\033[J"
 TEXT_TO_ALTERNATE = "\033[?1049h"
 TEXT_TO_NORMAL = "\033[?1049l"
-REQUEST_TERM_SIZE = "\033[18t"
+TEXT_BG_GREY = "\033[47m"
 
 class Highlight(str):
-    pass
+    def __repr__(self):
+        return "*%s*" % (str(self),)
 
 def get_proc_list(for_uid=None):
     for pid in os.listdir("/proc/"):
@@ -59,9 +62,17 @@ def re_search(proc_list, regex):
             match_str = (entry["cmd_line"][:match.start()], Highlight(match.group(0)), entry["cmd_line"][match.end():])
             yield dict(entry, match_str=match_str)
 
+def generic_search(proc_list, inp):
+    if inp and inp[0] == "/":
+        try:
+            return re_search(proc_list, inp[1:])
+        except:
+            return []
+    else:
+        return fuzzy_search(proc_list, inp)
 
-def format_output(candidate, width=None):
-    global_format = TEXT_DEFAULT
+def format_output(candidate, width=None, additional_format=""):
+    global_format = "%s%s" % (TEXT_DEFAULT, additional_format)
     match_str = []
     if candidate["owner"] != os.getuid():
         global_format = "%s%s" % (TEXT_DEFAULT, TEXT_HALF_BRIGHT)
@@ -89,63 +100,74 @@ def gentle_kill(candidates):
                 os.kill(entry["pid"], signal.SIGTERM)
             except OSError as e:
                 if e.errno == 3: # No such process
-                    continue
-                print("Failed to send SIGTERM to %s: %s" % (entry["cmd_line"], e))
+                    print("%s%s[%-5d]%s Was already gone: %s" % (TEXT_BOLD, TEXT_GREEN, entry["pid"], TEXT_DEFAULT, entry["cmd_line"]))
+                else:
+                    print("%s%s[%-5d]%s Failed to send SIGTERM to %s: %s" % (TEXT_BOLD, TEXT_RED, entry["pid"], TEXT_DEFAULT, entry["cmd_line"], e))
+                candidates.remove(entry)
 
         alive = 0
         for i in range(50):
             if i == 10:
                 if term_tries == 0:
-                    print("%d process(es) still alive despite SIGTERM, giving them 5 seconds to react before sending it again" % alive)
+                    print("%d process(es) still alive despite SIGTERM, giving them 5 seconds to react before sending it again" % len(candidates))
                 else:
-                    print("%d process(es) still alive despite SIGTERM, giving them 5 seconds to react before sending SIGKILL" % alive)
+                    print("%d process(es) still alive despite SIGTERM, giving them 5 seconds to react before sending SIGKILL" % len(candidates))
             time.sleep(.1)
             alive = 0
             for entry in candidates:
                 try:
                     os.kill(entry["pid"], 0)
-                    alive += 1
                 except OSError:
-                    pass
-            if not alive:
+                    print("%s%s[%-5d]%s Killed %s" % (TEXT_BOLD, TEXT_GREEN, entry["pid"], TEXT_DEFAULT, entry["cmd_line"]))
+                    candidates.remove(entry)
+            if not candidates:
                 break
-        if not alive:
+        if not candidates:
             break
 
-    if alive:
-        for entry in candidates:
-            try:
-                print("Still alive, sending SIGKILL to %s" % entry["cmd_line"])
-                os.kill(entry["pid"], signal.SIGKILL)
-            except OSError:
-                pass
+    if not candidates:
+        return
+
+    for entry in candidates:
+        try:
+            print("%s%s[%-5d]%s Sending SIGKILL to %s" % (TEXT_BOLD, TEXT_YELLOW, entry["pid"], TEXT_DEFAULT, entry["cmd_line"]))
+            os.kill(entry["pid"], signal.SIGKILL)
+        except OSError:
+            if e.errno == 3: # No such process
+                print("%s%s[%-5d]%s Was already gone: %s" % (TEXT_BOLD, TEXT_GREEN, entry["pid"], TEXT_DEFAULT, entry["cmd_line"]))
+            else:
+                print("%s%s[%-5d]%s Failed to send SIGKILL to %s: %s" % (TEXT_BOLD, TEXT_RED, entry["pid"], TEXT_DEFAULT, entry["cmd_line"], e))
+            candidates.remove(entry)
 
     for i in range(50):
         time.sleep(.1)
-        alive = 0
         for entry in candidates:
             try:
                 os.kill(entry["pid"], 0)
-                alive += 1
             except OSError:
+                print("%s%s[%-5d]%s Killed %s" % (TEXT_BOLD, TEXT_GREEN, entry["pid"], TEXT_DEFAULT, entry["cmd_line"]))
+                candidates.remove(entry)
                 pass
-        if not alive:
+        if not candidates:
             break
 
-    if alive:
-        print()
+    if candidates:
         for entry in candidates:
             try:
                 os.kill(entry["pid"], 0)
-                print("Failed to kill %s" % entry["cmd_line"])
+                print("%s%s[%-5d]%s Failed to SIGKILL %s" % (TEXT_BOLD, TEXT_RED, entry["pid"], TEXT_DEFAULT, entry["cmd_line"]))
             except OSError:
                 pass
 
-def main():
-    proc_list = tuple(get_proc_list(os.getuid()))
+def main_interactive(proc_list=None, initial_inp=""):
+    if not sys.stdout.isatty() or not sys.stdin.isatty():
+        print("Interactive mode requires a TTY on stdin/stdout")
+        return
+
+    proc_list = proc_list or tuple(get_proc_list(os.getuid()))
+    inp = initial_inp
 
     sys.stdout.write("".join((TEXT_TO_ALTERNATE, TEXT_BOLD, ">>> ", TEXT_DEFAULT)))
-    inp = ""
     opts = termios.tcgetattr(sys.stdin.fileno())
     copts = termios.tcgetattr(sys.stdin.fileno())
     copts[3] = copts[3] & ~termios.ICANON & ~termios.ECHO
@@ -153,12 +175,27 @@ def main():
     candidates = []
     try:
         cand_text = ""
+        selected = -1
         while True:
-            sys.stdout.write("".join((TEXT_ERASE, TEXT_BOLD, ">>> ", TEXT_DEFAULT,
+            cand_text = []
+            candidates = []
+            width, height = get_term_size()
+            if inp:
+                candidates = list(generic_search(proc_list, inp))
+                if selected >= min(len(candidates), height - 3):
+                    selected = min(len(candidates) - 1, height - 4)
+                for n, candidate in enumerate(candidates):
+                    cand_text.append(format_output(candidate, width, TEXT_BG_GREY if n == selected else ""))
+                    if n == height - 4:
+                        break
+            cand_text = "\n".join(cand_text)
+
+            sys.stdout.write("".join((TEXT_DEFAULT, TEXT_ERASE, TEXT_BOLD, ">>> ", TEXT_DEFAULT,
                                       inp if sys.version >= '3' else inp.encode(sys.getfilesystemencoding()),
                                       "\n", cand_text,
                                       "\033[%d;%dH" % (1, 5 + len(inp)))))
             sys.stdout.flush()
+
             if hasattr(sys.stdin, "buffer"):
                 char = sys.stdin.buffer.read1(10).decode(sys.getfilesystemencoding())
             else:
@@ -166,33 +203,22 @@ def main():
             if char == "\x7f":
                 if inp:
                     inp = inp[:-1]
+                selected = -1
             elif char == "\x1b":
                 candidates = []
                 break
             elif char[0] == "\x1b":
                 # Escape sequence
-                # [A -> up
-                # [B -> down
-                # [C -> right
-                # [D -> left
+                if char == "\x1b[A" and selected > -1: # Up
+                    selected -= 1
+                elif char == "\x1b[B": # Down
+                    selected += 1
                 pass
             elif char == "\n" or char == "\r":
                 break
             else:
+                selected = -1
                 inp += char
-
-            candidates = []
-            if inp:
-                if inp[0] == "/":
-                    try:
-                        candidates += re_search(proc_list, inp[1:])
-                    except:
-                        pass
-                else:
-                    candidates += fuzzy_search(proc_list, inp)
-
-            width, height = get_term_size()
-            cand_text = "\n".join(( format_output(candidate, width) for n, candidate in enumerate(candidates) if n < height - 3)) if candidates else ""
 
     except KeyboardInterrupt:
         inp = ""
@@ -202,7 +228,19 @@ def main():
         sys.stdout.write(TEXT_TO_NORMAL)
         sys.stdout.flush()
 
+    if selected > -1:
+        candidates = candidates[selected:selected+1]
+
     gentle_kill(candidates)
 
 if __name__ == '__main__':
-    main()
+    if sys.argv > 1:
+        inp = " ".join(sys.argv[1:])
+        proc_list = tuple(get_proc_list(os.getuid()))
+        candidates = list(generic_search(proc_list, inp))
+        if len(candidates) < 2:
+            gentle_kill(candidates)
+        else:
+            main_interactive(proc_list, inp)
+    else:
+        main_interactive()
